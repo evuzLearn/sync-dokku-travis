2642dd75bd87dbe6868b312549e5360331e11c22
evuz <jesus_jgb_45@hotmail.com>
2018-12-22 10:42:48 +0100

    feat: get activities function bot added

--- src/bot/functions/index.ts
+++ src/bot/functions/index.ts
@@ -3,9 +3,15 @@ import { EchoFunctionBot } from './EchoFunctionBot';
 import { StartFunctionBot } from './StartFunctionBot';
 import { ExpenseFunctionBot } from './ExpenseFunctionBot';
 import { FunctionBot } from './models/FunctionBot';
+import { GetActivitiesFunctionBot } from './GetActivitiesFunctionBot';
 
 export function addListeners({ bot }: { bot: Bot }) {
-  const botFunctions: FunctionBot[] = [new EchoFunctionBot(), new StartFunctionBot(), new ExpenseFunctionBot()];
+  const botFunctions: FunctionBot[] = [
+    new EchoFunctionBot(),
+    new StartFunctionBot(),
+    new ExpenseFunctionBot(),
+    new GetActivitiesFunctionBot(),
+  ];
 
   botFunctions.forEach(botFunction => {
     bot.addTextListener(botFunction.add());
