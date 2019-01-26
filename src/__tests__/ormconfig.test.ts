0f68ceb93227b05c4921eead73c56682fdf12310
Jesus Gómez <jesus_jgb_45@hotmail.com>
2019-01-26 10:17:34 +0100

    test: improve ormconfig test

--- src/__tests__/ormconfig.test.ts
+++ src/__tests__/ormconfig.test.ts
@@ -16,13 +16,13 @@ describe('ORM Config', () => {
   test('Should have db params', () => {
     expect.assertions(6);
     getOrmConfig().then(config => {
-      const configItems = Object.keys(config);
-      expect(configItems).toContain('url');
-      expect(configItems).toContain('username');
-      expect(configItems).toContain('password');
-      expect(configItems).toContain('database');
-      expect(configItems).toContain('host');
-      expect(configItems).toContain('port');
+      const configItems = Object.keys(config).map(key => ({ [key]: config[key] }));
+      expect(configItems).toContainEqual({ url: 'url' });
+      expect(configItems).toContainEqual({ username: 'user' });
+      expect(configItems).toContainEqual({ password: 'pass' });
+      expect(configItems).toContainEqual({ database: 'db' });
+      expect(configItems).toContainEqual({ host: 'host' });
+      expect(configItems).toContainEqual({ port: 'port' });
     });
   });
 
